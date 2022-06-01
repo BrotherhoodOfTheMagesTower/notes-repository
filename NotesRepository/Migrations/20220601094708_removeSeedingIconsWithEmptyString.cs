b460b5b0b6a2dd3e79b2a1aaac35afc59842c9ba
marekkawalski <marekaw987@student.polsl.pl>
2022-06-01 12:32:48 +0200

    [Fix]- now deleted folders don't show in noteMenu, [Fix]- remove seeding default note icons with an empty string, refactoring in NavMenu (delete redundant ToggleMainDirs method), minor layout changes in note and folder menu, now bin displays the same as folders

---
+++ NotesRepository/Migrations/20220601094708_removeSeedingIconsWithEmptyString.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotesRepository.Migrations
+{
+    public partial class removeSeedingIconsWithEmptyString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
