f00e6f91a7cff563e8fe2f7ba55d8185b66325f1
marekkawalski <marekaw987@student.polsl.pl>
2022-06-02 16:42:09 +0200

    add moving folders in folderMenu, refactor validation classes

--- NotesRepository/Data/Models/NoteValidation.cs
+++ NotesRepository/Data/Models/NoteValidation.cs
@@ -2,24 +2,14 @@
 
 namespace NotesRepository.Data.Models
 {
-    public class NoteValidation
+    public class NoteValidation : Validation
     {
-        public NoteValidation(string title, string emoji, string directory)
+        public NoteValidation(string title, string emoji, string directory) : base(title, directory)
         {
             Title = title;
             Directory = directory;
             Emoji = emoji;
         }
-
-        [Required(ErrorMessage = "Required field!")]
-        [StringLength(32, ErrorMessage = "Too long!")]
-        [MinLength(2, ErrorMessage = "Too short!")]
-        public string Title { get; set; }
-
-        [Required(ErrorMessage = "Required field!")]
-        [StringLength(32, ErrorMessage = "Too long!")]
-        [MinLength(2, ErrorMessage = "Too short!")]
-        public string Directory { get; set; }
 
         [Required(ErrorMessage = "Required field!")]
         public string Emoji { get; set; }
